f15e1cce495f419e8eae80792fa49e1717b900b7
Antons Andrejevs <anton.andreyeff@gmail.com>
2024-07-04 18:22:09 +0300

    firebase, appcheck

--- AmDm AI/AmDm_AIApp.swift
+++ AmDm AI/AmDm_AIApp.swift
@@ -6,9 +6,20 @@
 //
 
 import SwiftUI
+import Firebase
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        AppCheckManager.shared.setProviderFactory()
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct AmDm_AIApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+        
     var body: some Scene {
         WindowGroup {
             AppRoot()
